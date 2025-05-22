7c628916f6fd05e28558912907baf1702edc8d34
Santhosh2141 <snthshsrinivas@gmail.com>
2025-05-22 20:21:36 +0530

    Initial Commit

---
+++ WeSplit2/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  WeSplit2
+//
+//  Created by Santhosh Srinivas on 22/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("hi, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeSplit2/WeSplit2App.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplit2App.swift
+//  WeSplit2
+//
+//  Created by Santhosh Srinivas on 22/05/25.
+//
+
+import SwiftUI
+
+@main
+struct WeSplit2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
