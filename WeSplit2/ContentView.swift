ccaa164478aad01b33775c6c14b867da212bc52d
Santhosh2141 <snthshsrinivas@gmail.com>
2025-05-22 21:14:33 +0530

    Textfields, Pickers, ForEach

--- WeSplit2/ContentView.swift
+++ WeSplit2/ContentView.swift
@@ -10,15 +10,38 @@ import SwiftUI
 struct ContentView: View {
     // in structs we can use mutating func to update property. but for var in swiftUI we use @State
     @State private var tabCount = 0
+    @State private var name = ""
+    // this is a 2 way binding. we bind the name to the text field and when we update the textfield it should update the variable using $. no dollar means only read
+    
+    let detectives = ["Jake", "Amy", "Charles", "Rosa", "Terry"]
+    @State private var selectedDetective = "Jake"
     var body: some View {
+//        NavigationStack{
+//            Form{
+//                TextField("Enter your name", text: $name)
+//                Text("Your name is: \(name)")
+//                Button(
+//                "Tab Count \(tabCount)"
+//                ){
+//                    tabCount += 1
+//                }
+//                ForEach(0..<15) {
+//                    Text("Row number \($0+1)")
+//                }
+//            }
+//            .navigationTitle("SwiftUI")
+//            .navigationBarTitleDisplayMode(.inline)
+//        }
         NavigationStack{
-            Button(
-            "Tab Count \(tabCount)"
-            ){
-                tabCount += 1
+            Form{
+                Picker("Select your student", selection: $selectedDetective){
+                    // forEach detective, unique value is id which is itself in the array
+                    ForEach(detectives, id: \.self){
+                        Text($0)
+                    }
+                }
             }
-            .navigationTitle("SwiftUI")
-            .navigationBarTitleDisplayMode(.inline)
+            .navigationTitle("Select a Detective")
         }
     }
 }
